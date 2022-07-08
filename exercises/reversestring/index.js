af0d966bca2171885b5f5b93a9d761745e40fa62
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-07-07 23:07:41 -0700

    Add my first solution and second solution using JavaScript built in methods

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,23 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// My solution:
+// function reverse(str) {
+//   let reversedString = [];
+//   // Iterate through the entire length of the string starting from the end
+//   // We do so by letting i in the loop = length of the string
+//   // While i is greater than -1 we decrement its value though each iteration
+//   for (let i = str.length; i > -1; i--) {
+//     // As long as the value of the string is not an empty space
+//     if (str[i] !== '') {
+//       // We'll push the resulting value into variable reversed string
+//       // Note that push will insert the string value in the back of the array
+//       reversedString.push(str[i]);
+//     }
+//   }
+
+//   // Then return the array by joining the characters together
+//   return reversedString.join('');
+// }
+
+// Solution 2:
 function reverse(str) {
-  let reversedString = [];
+  
+}
 
-  // Iterate through the entire length of the string starting from the end
-  // We do so by letting i in the loop = length of the string
-  // While i is greater than -1 we decrement its value though each iteration
-  for (let i = str.length; i > -1; i--) {
-    // As long as the value of the string is not an empty space
-    if (str[i] !== '') {
-      // We'll push the resulting value into variable reversed string
-      // Note that push will insert the string value in the back of the array
-      reversedString.push(str[i]);
-    }
-  }
-
-  // Then return the array by joining the characters together
-  return reversedString.join('');
+// Solution 3: Using JavaScript built in reverse method
+function reverse(str) {
+  // const arrayString = str.split('');
+  // arrayString.reverse();
+  // return arrayString.join('');
+  return str.split('').reverse().join('');
 }
 
 module.exports = reverse;
