765d45aa1301a61088b730043ce7580c27d45d62
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-07-07 23:13:03 -0700

    Add third solution using for of loop syntax

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,29 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-// My solution:
-// function reverse(str) {
-//   let reversedString = [];
-//   // Iterate through the entire length of the string starting from the end
-//   // We do so by letting i in the loop = length of the string
-//   // While i is greater than -1 we decrement its value though each iteration
-//   for (let i = str.length; i > -1; i--) {
-//     // As long as the value of the string is not an empty space
-//     if (str[i] !== '') {
-//       // We'll push the resulting value into variable reversed string
-//       // Note that push will insert the string value in the back of the array
-//       reversedString.push(str[i]);
-//     }
-//   }
 
-//   // Then return the array by joining the characters together
-//   return reversedString.join('');
-// }
+// My solution:
+function reverse(str) {
+  let reversedString = [];
+  // Iterate through the entire length of the string starting from the end
+  // We do so by letting i in the loop = length of the string
+  // While i is greater than -1 we decrement its value though each iteration
+  for (let i = str.length; i > -1; i--) {
+    // As long as the value of the string is not an empty space
+    if (str[i] !== '') {
+      // We'll push the resulting value into variable reversed string
+      // Note that push will insert the string value in the back of the array
+      reversedString.push(str[i]);
+    }
+  }
+
+  // Then return the array by joining the characters together
+  return reversedString.join('');
+}
+
 
 // Solution 2:
 function reverse(str) {
-  
+  let reversed = '';
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
 }
+
 
 // Solution 3: Using JavaScript built in reverse method
 function reverse(str) {
