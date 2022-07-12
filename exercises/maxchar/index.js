8cc3328af049f0a85f2d16bb9db03acf14664750
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-07-11 17:44:03 -0700

    Add my solution to maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,33 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // create the object variable
+  let memory = {};
+  let maxChar = '';
+  let count = 0;
+
+  // iterating through the string
+  for (let char of str) {
+    // check if the character is in object variable
+    if (!memory[char]) {
+      // if not, assign the character to object with a value of 1
+      memory[char] = 1;
+    } else {
+      // otherwise we up the count
+      memory[char]++;
+    }
+  }
+  
+  // return the character with the highest value
+  for (let char in memory) {
+    if (memory[char] > count) {
+      maxChar = char;
+      count = memory[char];
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
