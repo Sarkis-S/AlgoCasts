851161a23a8fc6e660457270c4eb48da64baf406
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-07-12 19:45:38 -0700

    My solution to chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,45 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // create final output array
+  let output = [];
+  // create a temp array
+  let temp = [];
+  // create a count variable
+  let count = 0;
+
+  // iterate through the array
+  for (number of array) {
+    // find the last element
+    const last = array[array.length - 1];
+    
+    // immediately up the count
+    count++;
+
+    // if the count equals size
+    if (count === size) {
+      // push the temp array to output array
+      temp.push(number);
+      output.push(temp);
+
+      // reset count and temp
+      count = 0;
+      temp = [];
+    } else {
+      // check first if last element
+      if (number === last) {
+        temp.push(number);
+        output.push(temp);
+      } else {
+        // treat it like any number and push to temp
+        temp.push(number);
+      }
+    }
+  }
+
+  // return the output array
+  return output;
+}
 
 module.exports = chunk;
