b763990966c47d12f07a396d0240a7dfff5bf1a9
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-07-15 23:42:15 -0700

    Add second solution to capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -34,4 +34,15 @@ function capitalize(str) {
   return capitalized;
 }
 
+// Solution 2 (Simplified version)
+function capitalize(str) {
+  const words = [];
+
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(' ');
+}
+
 module.exports = capitalize;
