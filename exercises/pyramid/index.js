8e3191bfc6f946d398daf0acc03701f2e52d3634
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-07-18 20:01:57 -0700

    Add solution to pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,22 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const middle = Math.floor((2 * n - 1) / 2);
+  // iterate through array from 0 to n (rows)
+  for (let row = 0; row < n; row++) {
+    let step = '';
+    // iterate through column from 'both sides of n' (column)
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (middle - row <= column && middle + row >= column) {
+        step += '#';
+      } else {
+        step += ' ';
+      }
+    }
+
+    console.log(step);
+  }
+}
 
 module.exports = pyramid;
