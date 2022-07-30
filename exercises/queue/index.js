5918d2ea9fe4ada35d8b2b8aa75278159896c627
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-07-29 23:28:05 -0700

    Add Queue solution

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,20 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+// My note: in JavaScript we use unshift to put an element to the
+// start of an array, while pop removes it from the back
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
 
 module.exports = Queue;
