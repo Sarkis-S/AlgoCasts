07b666d0f482676fceacdb553dbc88c350bfcc49
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-08-03 23:45:57 -0700

    Add solution to qforms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,51 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
+// Stack is first record in, last record out
+// We don't want this behavior
 const Stack = require('./stack');
+// Que is first in, first out
+class Queue {
+  constructor() {
+    // creating an instance of a Queue will generate two stacks
+    this.first = new Stack();
+    this.second = new Stack();
+  }
 
-class Queue {}
+  add (record) {
+    this.first.push(record);
+  }
+
+  remove() {
+    // move everything to second stack
+    while (this.first.peek()) {
+      const record1 = this.first.pop();
+      this.second.push(record1);
+    }
+
+    const output = this.second.pop();
+    // move remaining record back to stack 1
+    while (this.second.peek()) {
+      const record2 = this.second.pop();
+      this.first.push(record2);
+    }
+    // return the desired value;
+    return output;
+  }
+
+  peek () {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    const record = this.second.peek();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record;
+  }
+}
 
 module.exports = Queue;
