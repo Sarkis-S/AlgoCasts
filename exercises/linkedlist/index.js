a89150e5100a0eac4f42707ed33720444eacf755
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-08-04 23:35:05 -0700

    Node setup for Linked List

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,7 +2,19 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+// Most basic LinkedList example:
+// const nodeOne = { data: 123 };
+// const nodeTwo = { data: 456 };
+// nodeOne.next = nodeTwo;
+// Linked List can be defined as two separate pieces of data
+// with some form of connection between them
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
 class LinkedList {}
 
