19fe4c5cc65675da161ae7c9bba9d1263d5a357c
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-08-05 22:42:51 -0700

    Implement insertFirst and size methods for LinkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -16,6 +16,33 @@ class Node {
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor () {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    // create a new Node and assign the current node to new Node's next value
+    // const node = new Node(data, this.head);
+    // now re-assign the head to point at new Node
+    // this.head = node;
+    // one line code equivalent:
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+    // check if there is a node currently
+    while (node) {
+      counter++;
+      // assign the next node to node variable for checking
+      node = node.next;
+    }
+
+    return counter;
+  }
+}
+
 
 module.exports = { Node, LinkedList };
