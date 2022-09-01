7ee0a2ec5098c5a8dfb950625dc4f935537dc6d5
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-08-31 18:19:20 -0700

    add notes to lengthWidth and solution to bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,46 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    // checks head value vs incoming value and if there is a node to the left of head
+    if (data < this.data && this.left) {
+      // if so apply recursion to the left until last node
+      this.left.insert(data);
+      // if this is the last node (left)
+    } else if (data < this.data) {
+      // then create node with data (left)
+      this.left = new Node(data);
+    // checks head value vs incoming value and if there is a node to the right of head
+    } else if (data > this.data && this.right) {
+      // if so apply recursion to the right until last node
+      this.right.insert(data);
+      // if this is the last node (right)
+    } else if (data > this.data) {
+      // create node with data (righ)
+      this.right = new Node(data);
+    }
+  }
+
+  contains(data) {
+    if (this.data === data) {
+      return this; // return node
+    }
+
+    if (this.data < data && this.right) {
+      return this.right.contains(data);
+    } else if (this.data > data && this.left) {
+      return this.left.contains(data);
+    }
+
+    return null;
+  }
+}
 
 module.exports = Node;
--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -12,17 +12,25 @@
 // Answer: [1, 3, 2]
 
 function levelWidth(root) {
+  // initialize our array with root and stopper
   const arr = [root, 's'];
-  const counters = [0]
-
+  // initialize counter array with zero
+  const counters = [0];
+  // make sure the array isn't empty otherwise
+  // will cause inifinite loop with 's' stoppper
   while (arr.length > 1) {
+    // take the node off the array
     const node = arr.shift();
-
+    // if its a stopper
     if (node === 's') {
+      // increase the counter
       counters.push(0);
+      // and push stopper back into the array in the back
       arr.push('s');
     } else {
+      // otherwise push all elements beind node
       arr.push(...node.children);
+      // then increase the latest counter
       counters[counters.length -1]++;
     }
   }
